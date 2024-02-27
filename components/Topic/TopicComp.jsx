e182e4d01b0f24dbeafa392089f8bd5b03728c9b
minhphonghp2003 <minhphonghp2003@gmail.com>
2024-02-27 10:56:45 +0700

    fix

--- components/Topic/TopicComp.jsx
+++ components/Topic/TopicComp.jsx
@@ -6,7 +6,7 @@ function TopicComp({ topic }) {
         <Link href={`/category/${topic.id}`}>
             <div className='p-9 pb-6  shadow-[1px_1px_8px_#00000014] hover:shadow-md duration-200 rounded-lg '>
                 <div className='flex gap-8 mb-8'>
-                    <img className='self-center w-[1.8rem] h-[2.8rem]' src={topic.icon} />
+                    <img className='self-center  h-[2.8rem]' src={topic.icon} />
                     <p className='self-start text-verticletitle font-[600]'> {topic.name}</p>
                 </div>
                 <p> {topic.description}</p>
